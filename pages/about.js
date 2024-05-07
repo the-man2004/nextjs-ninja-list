1b12647549c65d1f6b3e5ad9c9f8ed3bcc52532b
the-man2004 <heinrichhb111@gmail.com>
2024-05-07 14:51:28 -0800

    Added meta data to the home & about components.

--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,28 @@
+import Head from "next/head";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-        aliquid aspernatur voluptates. Consequatur, pariatur dolorem recusandae
-        non fugit cupiditate consectetur minima expedita, in voluptatum dolores
-        maiores obcaecati doloremque facilis vero!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-        aliquid aspernatur voluptates. Consequatur, pariatur dolorem recusandae
-        non fugit cupiditate consectetur minima expedita, in voluptatum dolores
-        maiores obcaecati doloremque facilis vero!
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+          aliquid aspernatur voluptates. Consequatur, pariatur dolorem
+          recusandae non fugit cupiditate consectetur minima expedita, in
+          voluptatum dolores maiores obcaecati doloremque facilis vero!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+          aliquid aspernatur voluptates. Consequatur, pariatur dolorem
+          recusandae non fugit cupiditate consectetur minima expedita, in
+          voluptatum dolores maiores obcaecati doloremque facilis vero!
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -8,23 +8,29 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-        aliquid aspernatur voluptates. Consequatur, pariatur dolorem recusandae
-        non fugit cupiditate consectetur minima expedita, in voluptatum dolores
-        maiores obcaecati doloremque facilis vero!
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-        aliquid aspernatur voluptates. Consequatur, pariatur dolorem recusandae
-        non fugit cupiditate consectetur minima expedita, in voluptatum dolores
-        maiores obcaecati doloremque facilis vero!
-      </p>
-      <Link href="/ninjas" className={styles.btn}>
-        Sea Ninja Listing
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+          aliquid aspernatur voluptates. Consequatur, pariatur dolorem
+          recusandae non fugit cupiditate consectetur minima expedita, in
+          voluptatum dolores maiores obcaecati doloremque facilis vero!
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+          aliquid aspernatur voluptates. Consequatur, pariatur dolorem
+          recusandae non fugit cupiditate consectetur minima expedita, in
+          voluptatum dolores maiores obcaecati doloremque facilis vero!
+        </p>
+        <Link href="/ninjas" className={styles.btn}>
+          Sea Ninja Listing
+        </Link>
+      </div>
+    </>
   );
 }
